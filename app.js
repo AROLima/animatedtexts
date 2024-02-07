aaf22d02c783aa4538b29d6859a13f9ea1288966
AroLima <rodrigo@Kyrian>
2024-02-06 21:34:33 -0300

    finished animations

--- app.js
+++ app.js
@@ -112,3 +112,18 @@ tl.fromTo(
     },
     '<'
     )
+
+
+//split alternative 
+
+
+const logo = document.querySelector('.logo')
+const letters = logo.textContent.split("")
+ logo.textContent = ""
+
+letters.forEach(letter => {
+    logo.innerHTML += "<span class='letter'>" + letter + '</span>'
+})
+
+gsap.set('.letter', { display: "inline-block" })
+gsap.fromTo('.letter', {y: '100%'}, {y: 0, delay: 2, stagger: 0.05, ease: 'back.out(3)'})
